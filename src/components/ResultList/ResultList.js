5aa84063506ad78cece9835875b502139f7dad62
Josh8903 <thorngren87@gmail.com>
2020-11-19 21:13:05 -0600

    cosmetic upgrades

--- src/components/ResultList/ResultList.js
+++ src/components/ResultList/ResultList.js
@@ -3,36 +3,40 @@ import "./style.css";
 
 function ResultList(props) {
   return (
-    <table className="table table-striped table-hover">
-      <thead>
-        <tr id="tableHeader" className="bg-primary">
-          <th scope="col">Profile Photo</th>
-          <th scope="col" onClick={props.handleSort}>
-            Name
-          </th>
-          <th scope="col">Phone Number</th>
-          <th scope="col">Email</th>
-          <th scope="col">Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.results.map((result, index) => {
-          return (
-            <tr key={index}>
-              <td>
-                <img alt="Profile" src={result.picture.thumbnail} />
-              </td>
-              <td>
-                {result.name.first} {result.name.last}
-              </td>
-              <td>{result.phone}</td>
-              <td>{result.email}</td>
-              <td>{result.location.city}</td>
+    <div className="container">
+      <div id="tableWrapper" className="row">
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr id="tableHeader" className="bg-warning">
+              <th scope="col">Profile Photo</th>
+              <th scope="col" onClick={props.handleSort}>
+                Name
+              </th>
+              <th scope="col">Phone Number</th>
+              <th scope="col">Email</th>
+              <th scope="col">Location</th>
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          </thead>
+          <tbody>
+            {props.results.map((result, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    <img alt="Profile" src={result.picture.thumbnail} />
+                  </td>
+                  <td>
+                    {result.name.first} {result.name.last}
+                  </td>
+                  <td>{result.phone}</td>
+                  <td>{result.email}</td>
+                  <td>{result.location.city}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,27 +3,31 @@ import "./style.css";
 
 function SearchForm(props) {
   return (
-    <form id="search-group">
-      <div className="input-group mb-3">
-        <input
-          onChange={props.handleInputChange}
-          value={props.search}
-          name="search"
-          type="text"
-          className="form-control"
-          placeholder="Search for employees"
-          id="searchEmployees"
-        />
-        <div className="input-group-append">
-          <button
-            onClick={props.handleFormSubmit}
-            className="btn btn-primary mt-3"
-          >
-            Search
-          </button>
-        </div>
+    <div className="container">
+      <div id="searchWrapper" className="row">
+        <form id="search-group">
+          <div className="input-group mb-3">
+            <input
+              onChange={props.handleInputChange}
+              value={props.search}
+              name="search"
+              type="text"
+              className="form-control"
+              placeholder="Search for employees"
+              id="searchEmployees"
+            />
+            <div className="input-group-append">
+              <button
+                onClick={props.handleFormSubmit}
+                className="btn btn-warning mt-3"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
+    </div>
   );
 }
 
